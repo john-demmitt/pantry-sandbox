16df0d8dad828f351d8d898045cd601f09dcd209
Unknown <john_demmitt@outlook.com>
2017-10-05 05:25:17 -0700

    css selector stuff

--- selectorlist.js
+++ selectorlist.js
@@ -6,17 +6,17 @@ var listSelectors = require('list-selectors');
  
 
   listSelectors(
-  ['css/pantry-new-menu-svg.css'], // source 
+  ['buttons/simple-radial-menu/css/style.css'], // source 
   // options 
   function(myList) { // callback 
-    fs.writeFile('css/pantry-new-menu-svg.css.txt', JSON.stringify(myList, undefined, 2),
+    fs.writeFile('radial-menu-style.css.txt', JSON.stringify(myList, undefined, 2),
     function (err) {
      if (err) return console.log(err);
-     console.log('theList > css/pantry-new-menu-svg.css.txt');
+     console.log('theList > css\radial-menu-style.css.txt');
     console.log(myList);
     // ... do other things with your nice selector list
    });
 
-   
+  
   });
    
